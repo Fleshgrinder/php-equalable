45500ead60d3a87a17beebf390f8aeb26c61b2ed
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2015-09-23 19:11:45 +0200

    Removed cast in dummy, could be useful to return anything.

--- test/EqualableDummy.php
+++ test/EqualableDummy.php
@@ -12,8 +12,7 @@ namespace Fleshgrinder\Core;
  *
  * This class is provided for tests that want to verify that something accepts or is able to handle instances that
  * implement the {@see Equalable} interface. This class does not contain any logic and the boolean that is returned by
- * the equals method is simply a public property that can be altered by anyone. The method performs and additional
- * boolean cast to ensure that the returned value is actually a boolean, as anticipated by a caller.
+ * the equals method is simply a public property that can be altered by anyone.
  *
  * This class is final because nobody should ever extend it, instead implement the {@see Equalable} interface.
  */
@@ -28,7 +27,7 @@ final class EqualableDummy implements Equalable {
      * @inheritDoc
      */
     public function equals($value) {
-        return (boolean) $this->equals;
+        return $this->equals;
     }
 
 }
